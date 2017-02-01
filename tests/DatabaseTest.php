7cb94834b7997e32ef1b8f10ef4a9335f21f6fdd
Mark Topper <mark@ulties.com>
2017-02-01 15:05:55 +0100

    Remove test users

--- tests/DatabaseTest.php
+++ tests/DatabaseTest.php
@@ -262,8 +262,6 @@ class DatabaseTest extends TestCase
 
     public function test_can_create_nullable_column()
     {
-        dd(\TCG\Voyager\Models\User::all());
-
         Auth::loginUsingId(1);
 
         // Create table
